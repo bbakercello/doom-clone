f6d65ec4d393d9abd67e61637d9e59d02cf579bd
bbakercello <bbakercello@gmail.com>
2022-12-02 10:41:51 -0500

    mini map created

---
+++ player.py
@@ -0,0 +1,12 @@
+from settings import * 
+import pygame as pg
+import math
+
+
+class Player:
+    def __init__(self,game):
+        self.game = game 
+        self.x, self.y = PLAYER_POS 
+        self.angle = PLAYER_ANGLE 
+
+    def moveme
